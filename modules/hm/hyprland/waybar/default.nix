b2b28d50ca49d2caa5bebcbd0e1071172b6d463b
SanoLitch <sanolitch@gmail.com>
2026-01-04 03:17:06 +0300

    fix waybar

--- modules/hm/hyprland/waybar/default.nix
+++ modules/hm/hyprland/waybar/default.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   imports = [ ./resolution-module.nix ];
 
   xdg.configFile."waybar/modules/custom-clock.jsonc".source = ./custom-clock.jsonc;
   xdg.configFile."waybar/modules/custom-language.jsonc".source = ./custom-language.jsonc;
-  xdg.configFile."waybar/layouts/custom.jsonc".source = ./custom-layout.jsonc;
-}
 
+  xdg.configFile."waybar/layouts/custom.jsonc".source = ./custom-layout.jsonc;
+
+  home.file.".config/waybar/config.jsonc" = lib.mkForce {
+    source = ./custom-layout.jsonc;
+    force = true;
+  };
+}
--- modules/hm/hyprland/waybar/resolution-module.nix
+++ modules/hm/hyprland/waybar/resolution-module.nix
@@ -7,19 +7,18 @@ let
 
     if [ "$CURRENT" = "3440" ]; then
         ${pkgs.hyprland}/bin/hyprctl keyword monitor "${monitorDesc}, 2560x1080@144, auto, 1.333334"
-        ${pkgs.libnotify}/bin/notify-send "Resolution" "Switched to 2560x1080"
     else
         ${pkgs.hyprland}/bin/hyprctl keyword monitor "${monitorDesc}, 3440x1440@144, auto, 1.6"
-        ${pkgs.libnotify}/bin/notify-send "Resolution" "Switched to Default"
     fi
   '';
 
   resCheck = pkgs.writeShellScript "res-check" ''
     CURRENT=$(${pkgs.hyprland}/bin/hyprctl monitors -j | ${pkgs.jq}/bin/jq -r '.[] | select(.description | contains("Xiaomi")) | .width')
+
     if [ "$CURRENT" = "3440" ]; then
         echo '{"text": "1440p", "tooltip": "Resolution: 3440x1440\nScale: 1.6"}'
     else
-        echo '{"text": "1080p", "tooltip": "Resolution: 2560x1080\nScale: 1.6"}'
+        echo '{"text": "1080p", "tooltip": "Resolution: 2560x1080\nScale: 1.333334"}'
     fi
   '';
 
@@ -35,5 +34,5 @@ let
   };
 in
 {
-  xdg.configFile."waybar/modules/resolution.jsonc".text = builtins.toJSON resolutionModule;
+  xdg.configFile."waybar/modules/custom-resolution.jsonc".text = builtins.toJSON resolutionModule;
 }
